410e80be2f677a46c656f6d8b4a83801de88f67f
Nikita Gryzlov <nixel2007@gmail.com>
2021-05-13 17:46:21 +0300

    deps bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,41 +2,39 @@ import java.net.URI
 
 plugins {
     java
-    maven
+    `maven-publish`
     jacoco
-    id("io.franzbecker.gradle-lombok") version "3.3.0"
+    id("io.freefair.lombok") version "6.0.0-m2"
     id("com.github.gradle-git-version-calculator") version "1.1.0"
+    id("com.github.ben-manes.versions") version "0.38.0"
 }
 
-group = "com.github.1c-syntax"
+group = "io.github.1c-syntax"
 version = gitVersionCalculator.calculateVersion("v")
 
-val junitVersion = "5.5.2"
+val junitVersion = "5.8.0-M1"
 
 repositories {
     mavenCentral()
-    maven { url = URI("https://jitpack.io") }
+    maven(url = "https://jitpack.io")
 }
 
 dependencies {
 
     implementation("org.apache.commons", "commons-collections4", "4.4")
-    compileOnly("org.projectlombok", "lombok", lombok.version)
 
     // тестирование
     testImplementation("org.junit.jupiter", "junit-jupiter-api", junitVersion)
     testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", junitVersion)
-    testImplementation("org.assertj", "assertj-core", "3.12.2")
-    testImplementation("com.ginsberg", "junit5-system-exit", "1.0.0")
-}
-
-configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_11
+    testImplementation("org.assertj", "assertj-core", "3.19.0")
+    testImplementation("com.ginsberg", "junit5-system-exit", "1.1.1")
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
+    withSourcesJar()
+    withJavadocJar()
 }
 
 tasks.test {
@@ -53,10 +51,4 @@ tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
     options.compilerArgs.add("-Xlint:unchecked")
     options.compilerArgs.add("-parameters")
-}
-
-
-lombok {
-    version = "1.18.12"
-    sha256 = "49381508ecb02b3c173368436ef71b24c0d4418ad260e6cc98becbcf4b345406"
 }
